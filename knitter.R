b9a717b7d11f724841d1ae7d9770982f663a8a16
Brandevin <felipe.courasuzuki@gmail.com>
2021-04-02 23:57:06 -0300

    Finished adding descriptions. Polished the markdown file. Added image references and organized folder files

--- knitter.R
+++ knitter.R
@@ -1,6 +1,6 @@
 library(knitr)
 library(rmarkdown)
 setwd()
-knit(input='main_real_state_notebook.Rmd')
-render(input='main_real_state_notebook.md',output_format='html_document')
+knit(input='main_real_state_notebook_code.Rmd')
+render(input='main_real_state_notebook_code.md',output_format='html_document')
 
